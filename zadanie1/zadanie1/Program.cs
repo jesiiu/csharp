970d7dd3a15fc448ff03412300c31fda7b33a99d
jesiiu <jesiiuyt@gmail.com>
2022-02-05 16:27:09 +0100

    Add files via upload

---
+++ zadanie1/zadanie1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+
+Console.Write("Insert your EAN-13: ");
+
+
+string UserEAN = Console.ReadLine();
+char[] ean = UserEAN.ToCharArray();
+int[] eanValue = Array.ConvertAll(ean, c => (int)Char.GetNumericValue(c));
+
+if(UserEAN.Length == 13)
+{ 
+int sum()
+{
+    int suma = 1 * eanValue[0] + 3 * eanValue[1] + 1 * eanValue[2] + 3 * eanValue[3] + 1 * eanValue[4] + 3 * eanValue[5] + 1 * eanValue[6] + 3 * eanValue[7] + 1 * eanValue[8] + 3 * eanValue[9] + 1 * eanValue[10] + 3 * eanValue[11];
+    
+    if (suma % 10 == 0)
+    {
+        return suma-suma;
+    }
+    else
+    {
+        return (10 * ((suma + 9) / 10))-suma;
+    }
+    
+}
+
+    int suma = sum();
+    Console.WriteLine($"EAN check value: {suma} is valid");
+}
+else
+{
+    Console.WriteLine("Invalid EAN");
+}
+
+
+ 
+   
+
+
+
+
+
+
+
+
