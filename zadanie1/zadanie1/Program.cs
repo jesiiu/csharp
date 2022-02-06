ff490e201b2759246e50ca170c1c04638178ad8c
jesiiu <jesiiuyt@gmail.com>
2022-02-06 20:56:49 +0100

    Update Program.cs

--- zadanie1/zadanie1/Program.cs
+++ zadanie1/zadanie1/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections.Generic;
 
-
+//Pobranie kodu EAN od użytkownika
 Console.Write("Insert your EAN-13: ");
 
 
 string UserEAN = Console.ReadLine();
+//Zamiana wprowadzonego kodu na tablice char a potem na int
 char[] ean = UserEAN.ToCharArray();
 int[] eanValue = Array.ConvertAll(ean, c => (int)Char.GetNumericValue(c));
-
+//Sprawdzenie ze wprowadzony kod ma 13 znaków
 if(UserEAN.Length == 13)
 { 
 int sum()
 {
+    //Logika na obliczenie cyfry kontrolnej
     int suma = 1 * eanValue[0] + 3 * eanValue[1] + 1 * eanValue[2] + 3 * eanValue[3] + 1 * eanValue[4] + 3 * eanValue[5] + 1 * eanValue[6] + 3 * eanValue[7] + 1 * eanValue[8] + 3 * eanValue[9] + 1 * eanValue[10] + 3 * eanValue[11];
     
     if (suma % 10 == 0)
@@ -24,12 +26,13 @@ int sum()
     }
     
 }
-
+//Wypisanie informacji dla użytkownika czy kod jest prawidłowy
     int suma = sum();
     Console.WriteLine($"EAN check value: {suma} is valid");
 }
 else
 {
+    //Wypisanie informacji o błednym wprowadzonym kodzie EAN
     Console.WriteLine("Invalid EAN");
 }
 
